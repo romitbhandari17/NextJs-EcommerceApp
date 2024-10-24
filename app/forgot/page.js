522e49e9723275a1146721a3e45741a44329c259
Romit <romitbhandari17@gmail.com>
2024-10-24 17:44:33 +0530

    login page

---
+++ app/forgot/page.js
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Forgot = () => {
+    return (
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img className="mx-auto h-10 w-auto" src="/images/redlily-logo.jpg" alt="Redlily" />
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Forgot Password</h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" action="#" method="POST">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <div className="mt-2">
+                            <input id="email" name="email" type="email" autoComplete="Enter your email address" required className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6" />
+                        </div>
+                    </div>
+                    <div>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Forgot Password</button>
+                    </div>
+                </form>
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    Not a member?
+                    <Link href={"/signup"} className="font-semibold leading-6 text-red-600 hover:text-red-500">Sign-Up</Link>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Forgot
