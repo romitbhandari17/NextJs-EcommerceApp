b57c160586ac36883673e312b08ede7f53fcb3df
Romit <romitbhandari17@gmail.com>
2024-10-24 13:11:33 +0530

    order page

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -14,35 +14,35 @@ const Checkout = () => {
       <div className="m-auto flex my-1">
         <div className="w-1/2 p-2">
           <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-          <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
         <div className="w-1/2 p-2">
           <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-          <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
       </div>
       <div className="w-full p-2 my-1">
         <label htmlFor="address" className="leading-7 text-sm text-gray-600">Address</label>
-        <textarea name="address" id="address" cols="30" rows="2" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
+        <textarea name="address" id="address" cols="30" rows="2" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
       </div>
       <div className="m-auto flex my-1">
         <div className="w-1/2 p-2">
           <label htmlFor="phone" className="leading-7 text-sm text-gray-600">Phone</label>
-          <input type="text" id="phone" name="phone" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <input type="text" id="phone" name="phone" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
         <div className="w-1/2 p-2">
           <label htmlFor="city" className="leading-7 text-sm text-gray-600">City</label>
-          <input type="text" id="city" name="city" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <input type="text" id="city" name="city" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
       </div>
       <div className="m-auto flex my-1">
         <div className="w-1/2 p-2">
           <label htmlFor="state" className="leading-7 text-sm text-gray-600">State</label>
-          <input type="text" id="state" name="state" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <input type="text" id="state" name="state" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
         <div className="w-1/2 p-2">
           <label htmlFor="pincode" className="leading-7 text-sm text-gray-600">Pincode</label>
-          <input type="text" id="pincode" name="pincode" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <input type="text" id="pincode" name="pincode" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
       </div>
       <h2 className='text-xl font-semibold mx-2'>2. Review Cart Items:</h2>
--- app/order/page.js
+++ app/order/page.js
@@ -2,7 +2,47 @@ import React from 'react'
 
 const Order = () => {
   return (
-    <div>Order</div>
+    <div><section className="text-gray-600 body-font overflow-hidden">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="lg:w-4/5 mx-auto flex flex-wrap">
+          <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+            <h2 className="text-sm title-font text-red-500 tracking-widest">REDLILY</h2>
+            <h1 className="text-red-900 text-3xl title-font font-medium mb-4">OrderId: #14456</h1>
+            {/* <div className="flex mb-4">
+              <a className="flex-grow text-red-500 border-b-2 border-red-500 py-2 text-lg px-1">Description</a>
+              <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Reviews</a>
+              <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Details</a>
+            </div> */}
+            <h3 className="text-gray-500 text-sm title-font font-medium mb-4">Your Order has been successfully placed.</h3>
+            <div className="flex border-t border-gray-200 py-2 space-x-2 justify-between">
+              <div className="text-red-600 w-2/3">Description</div>
+              <div className="ml-auto text-red-600">Qty</div>
+              <div className="ml-auto text-red-600">Item Price</div>
+            </div>
+            <div className="flex border-t border-gray-200 py-2 space-x-2 justify-between">
+              <div className="text-red-500 text-sm w-2/3">Red Bandhej Ethinc Kurta Set with Pant (XL/Blue)</div>
+              <div className="ml-auto text-gray-900">1</div>
+              <div className="ml-auto text-gray-900">499</div>
+            </div>
+            <div className="flex border-t border-gray-200 py-2 space-x-2 justify-between">
+              <div className="text-red-500 text-sm w-2/3">Red Bandhej Ethinc Kurta Set with Pant (L/Red)</div>
+              <div className="ml-auto text-gray-900">1</div>
+              <div className="ml-auto text-gray-900">499</div>
+            </div>
+            <div className="flex border-t border-gray-200 py-2 space-x-2 justify-between">
+              <div className="text-red-500 text-sm w-2/3">Red Bandhej Ethinc Kurta Set with Pant (XL/Red)</div>
+              <div className="ml-auto text-gray-900">1</div>
+              <div className="ml-auto text-gray-900">499</div>
+            </div>
+            <div className="flex">
+              <div className="title-font font-medium text-2xl text-gray-900">₹ 1497</div>
+              <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Track Order</button>
+            </div>
+          </div>
+          <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400"/>
+        </div>
+      </div>
+    </section></div>
   )
 }
 
